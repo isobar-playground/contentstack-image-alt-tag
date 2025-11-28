00c1adfeef5efbd66b1634893ba66eb81573069e
Marcin Maruszewski <marcin.maruszewski@isobar.com>
2025-11-28 22:27:24 +0100

    feat: configure Next.js image loader for Contentstack assets.

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    // Add remotePatterns if you are using external images, e.g.:
-    // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'example.com',
-    //     port: '',
-    //     pathname: '/my-images/**',
-    //   },
-    // ],
-  },
-  compiler: {
-    // Remove console.log in production
-    removeConsole: process.env.NODE_ENV === "production",
-  },
-  experimental: {
-    serverActions: {},
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.contentstack.io',
+        port: '',
+        pathname: '/v3/assets/**',
+      },
+    ],
   },
 };
 
 export default nextConfig;
-
